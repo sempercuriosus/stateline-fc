c396585d2ea56f351f10982745778100aa6fbf22
sempercuriosus <vespers-garages.0n@icloud.com>
2024-01-19 05:24:45 -0500

    updated layout of nav

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -4,6 +4,7 @@ function Nav() {
       className='navbar is-info'
       role='navigation'
       aria-label='main navigation'>
+      {/* Brand / Logo */}
       <div className='navbar-brand'>
         <a
           className='navbar-item'
@@ -30,32 +31,38 @@ function Nav() {
       <div
         id='navbarBasicExample'
         className='navbar-menu'>
+        {/* Start */}
         <div className='navbar-start'>
+          {/* Home */}
           <a className='navbar-item'>Home</a>
 
-          <a className='navbar-item'>Contact</a>
-
-          <a className='navbar-item'>About</a>
-
+          {/* Dropdown */}
           <div className='navbar-item has-dropdown is-hoverable'>
             <a className='navbar-link'>Services</a>
-
             <div className='navbar-dropdown'>
-              <a className='navbar-item'>Service 1</a>
-              <a className='navbar-item'>Service 2</a>
-              <a className='navbar-item'>Service 3</a>
+              <a className='navbar-item'>Pediatric</a>
+              <a className='navbar-item'>Family</a>
+              <a className='navbar-item'>Prenatal</a>
+              {/* <a className='navbar-item'></a> */}
               <hr className='navbar-divider' />
               <a className='navbar-item'>FAQs</a>
             </div>
           </div>
+
+          {/* Contact */}
+          <a className='navbar-item'>Contact</a>
+
+          {/* About */}
+          <a className='navbar-item'>About</a>
         </div>
 
         <div className='navbar-end'>
           <div className='navbar-item'>
-            <p>A Thing</p>
+            <p>Schedule An Appointment</p>
           </div>
         </div>
       </div>
+      {/* End */}
     </nav>
   );
 }
