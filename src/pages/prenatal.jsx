169502e8370d1caa6fe7cccb30fcbba8737c0d1a
sempercuriosus <vespers-garages.0n@icloud.com>
2024-01-19 18:31:16 -0500

    added page content

--- src/pages/prenatal.jsx
+++ src/pages/prenatal.jsx
@@ -1,7 +1,43 @@
 function Prenatal() {
   return (
     <div className='section'>
-      <p> Prenatal Display Test </p>
+      <p>
+        Taking care of pregnant moms means taking care of the whole family. The
+        journey during preconception, pregnancy, labor and delivery and the
+        postpartum period can affect the health of the entire family.
+      </p>
+      <p>
+        In today’s healthcare, pregnancy is so medicalized that mom’s don’t
+        trust their bodies to do what God intended. They are led to believe that
+        their provider knows better that the mom about what’s going on in her
+        own body.
+      </p>
+      <p>
+        This view of pregnancy leaves moms with stress, anxiety, exhaustion and
+        other challenges during pregnancy instead of feeling empowered and
+        supported.
+      </p>
+      <p>
+        At Stateline Family Chiropractic, we strive to provide moms with
+        adjustments (obviously), support and empowerment that they so need and
+        deserve!
+      </p>
+      <p>
+        Chiropractic care during pregnancy and postpartum periods reduces
+        tension, boosts energy, can help baby achieve ideal positioning and even
+        help with an easier delivery and potentially decrease chances of
+        PPD/PPA.
+      </p>
+      <p>
+        Our care doesn’t stop the day your beautiful baby arrives. We come right
+        alongside you in that next critical period of your family's life,
+        providing incredible care and support for both mom and baby.
+      </p>
+      <p>
+        Dr. Katie is highly trained in Webster’s Technique (among others) and
+        experienced in taking great care of mom before, during and after
+        pregnancy.
+      </p>
     </div>
   );
 }
