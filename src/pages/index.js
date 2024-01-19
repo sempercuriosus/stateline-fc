5ff6236e8727cfb49c823ea936c7d0a573456342
sempercuriosus <vespers-garages.0n@icloud.com>
2024-01-19 18:07:25 -0500

    init

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+export { default as Intro } from './intro';
+export { default as About } from './about';
+export { default as Contact } from './contact';
+export { default as Services } from './services';
+export { default as FAQ } from './faq';
+export { default as Prenatal } from './prenatal';
+export { default as Pediatric } from './pediatric';
+export { default as Family } from './family';
+export { default as NewPatient } from './newPatient';
+
