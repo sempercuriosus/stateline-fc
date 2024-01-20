1f7ae286716086b57cd3f3f27b92331242616820
sempercuriosus <vespers-garages.0n@icloud.com>
2024-01-20 06:15:10 -0500

    setting up routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Fragment } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import { Intro, About, Services, NewPatient } from './pages';
 // import Services from './pages/services.jsx';
 // import Contact from './pages/contact.jsx';
@@ -17,6 +18,15 @@ function App() {
 
         <About />
       </main>
+      <Routes>
+        <Route>Home</Route>
+        <Route>About</Route>
+        <Route>Contact</Route>
+        <Route>Pediatric</Route>
+        <Route>Prenatal</Route>
+        <Route>Family</Route>
+        <Route></Route>
+      </Routes>
     </Fragment>
   );
 }
