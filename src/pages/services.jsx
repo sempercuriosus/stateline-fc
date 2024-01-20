aae87efa109a74bee7a1a5c805fe86203f62c9d7
sempercuriosus <vespers-garages.0n@icloud.com>
2024-01-20 11:39:29 -0500

    updated content with card

--- src/pages/services.jsx
+++ src/pages/services.jsx
@@ -1,51 +1,46 @@
+import ServiceCard from './service-card';
+
 function Services() {
   return (
-    <div className='section'>
-      <h2 className='title has-text-centered'>What We Offer</h2>
+    <section className='section'>
+      <h2 className='title has-text-centered block'>What We Offer</h2>
 
       {/* Column Container */}
       <div className='columns'>
-        {/* Column */}
-        <div className='column has-ratio'>
-          <figure className='image is-128x128'>
-            <img src='https://bulma.io/images/placeholders/128x128.png' />
-          </figure>
+        {/*  */}
 
-          <div className='block'>
-            <h3 className='title is-4'>Pediatric </h3>
-            Kids need chiropractic too! We’ve had great results with colicky
-            infants to chronically sick kids to sensory and spectrum challenges.
-          </div>
-        </div>
+        {/* Service Prenatal */}
 
-        {/* Column */}
-        <div className='column has-ratio'>
-          <figure className='image is-128x128'>
-            <img src='https://bulma.io/images/placeholders/128x128.png' />
-          </figure>
+        <ServiceCard
+          serviceTitle='Prenatal'
+          serviceDescription='Family health starts in the prenatal period! We strive to empower
+            and support moms from conception to birth and beyond'
+          serviceImageSource='https://bulma.io/images/placeholders/128x128.png'
+          serviceImageAlt='Prenatal Image Placeholder'
+        />
 
-          <div className='block'>
-            <h3 className='title is-4'>Prenatal </h3>
-            Family health starts in the prenatal period! We strive to empower
-            and support moms from conception to birth and beyond
-          </div>
-        </div>
+        {/* Service - Pediatric */}
 
-        {/* Column */}
-        <div className='column has-ratio'>
-          <figure className='image is-128x128'>
-            <img src='https://bulma.io/images/placeholders/128x128.png' />
-          </figure>
+        <ServiceCard
+          serviceTitle='Pediatric'
+          serviceDescription='Kids need chiropractic too! We’ve had great results with colicky
+            infants to chronically sick kids to sensory and spectrum challenges.'
+          serviceImageSource='https://bulma.io/images/placeholders/128x128.png'
+          serviceImageAlt='Pediatric Image Placeholder'
+        />
 
-          <div className='block'>
-            <h3 className='title is-4'>Family</h3>
-            We love to support entire families on their health journey.
+        {/* Service - Family */}
+
+        <ServiceCard
+          serviceTitle='Family'
+          serviceDescription=' We love to support entire families on their health journey.
             Consistent care can help everyone sleep better, feel better and
-            enjoy life better, together!
-          </div>
-        </div>
+            enjoy life better, together!'
+          serviceImageSource='https://bulma.io/images/placeholders/128x128.png'
+          serviceImageAlt='Family Image Placeholder'
+        />
       </div>
-    </div>
+    </section>
   );
 }
 
