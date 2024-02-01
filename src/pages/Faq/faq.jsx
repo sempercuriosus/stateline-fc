de7580bfa85bf8962141941a31639a6b4d7f93db
sempercuriosus <vespers-garages.0n@icloud.com>
2024-02-01 06:51:47 -0500

    added content and style

--- src/pages/Faq/faq.jsx
+++ src/pages/Faq/faq.jsx
@@ -1,7 +1,81 @@
 function FAQ() {
   return (
-    <section className=' snap-stop'>
-      <p> FAQ Display Test </p>
+    <section className='section snap-stop'>
+      <h2 className='title'>Frequently Asked Questions</h2>
+
+      <div className='block'>
+        <p>
+          <strong>Do you take insurance?</strong>
+        </p>
+        <p>
+          In order to provide the best patient-focused care that we can, we do
+          not accept health insurance. We are happy to provide superbills upon
+          request for you to submit. HSA and FSA are accepted.{' '}
+        </p>
+      </div>
+
+      <div className='block'>
+        <p>
+          <strong>What techniques do you use?</strong>
+        </p>
+        <ul className='content'>
+          <li>Diversified</li>
+          <li>Gonstead</li>
+          <li>flexion-distraction</li>
+          <li>Thompson drop</li>
+          <li>Activator</li>
+          <li>Webster</li>
+        </ul>
+      </div>
+
+      <div className='block'>
+        <p>
+          <strong>Do you take x-rays?</strong>
+        </p>
+        <p>
+          We do have the ability to take x-rays in-office. Dr. Katie will
+          determine if they’re necessary at your appointment.{' '}
+        </p>
+      </div>
+
+      <div className='block'>
+        <p>
+          <strong>Can my child(ren) come to my appointment?</strong>
+        </p>
+        <p>
+          Of course! We have a family focus and are happy to have them come with
+          you. We have a kids area to help keep them occupied but feel free to
+          bring snacks to help too!{' '}
+        </p>
+      </div>
+
+      <div className='block'>
+        <p>
+          <strong>
+            I don’t see anything available when scheduling online, do you have
+            other appointments available?
+          </strong>
+        </p>
+        <p>
+          You bet! We open up what we can online but many times (especially for
+          new patients) we are able to get you scheduled at times that don’t
+          show up online.
+        </p>
+      </div>
+
+      <div className='block'>
+        <p>
+          <strong>
+            I want to bring my family in for wellness care. Do you have plans to
+            help make that affordable?
+          </strong>
+        </p>
+        <p>
+          Absolutely. Depending on your family size, we have wellness plans that
+          help make entire family care attainable. Call our office for more
+          information!
+        </p>
+      </div>
     </section>
   );
 }
