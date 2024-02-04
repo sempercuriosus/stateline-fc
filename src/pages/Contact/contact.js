b9c794a0dbf84b9ecfd787d9d9ce5318cc89dc02
sempercuriosus <vespers-garages.0n@icloud.com>
2024-02-04 12:42:01 -0500

    this was missing somehow?

--- src/pages/Contact/contact.js
+++ src/pages/Contact/contact.js
@@ -1,5 +1,24 @@
 const contactInfo = {
   phoneNumber: '803-358-8340',
+  email: 'statelinefamilychiro@proton.me',
+  hours: {
+    Monday: '3pm until 6pm',
+    Tuesday: '3pm until 6pm',
+    Wednesday: '3pm until 6pm',
+    Thursday: '3pm until 6pm',
+    Friday: '3pm until 6pm',
+    Saturday: '8am until 11am',
+    Sunday: 'Closed',
+    Delim: '-',
+  },
+  address: {
+    line1: '856 Gold Hill Road',
+    line2: '#103',
+    city: 'Fort Mill',
+    state: 'SC',
+    zip: '29708',
+    note: 'Located Inside Kane Family Chiropractic',
+  },
 };
 
 export default { contactInfo };
