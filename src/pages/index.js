4a7f95651c1dc375970798c9a96b991a55be639f
sempercuriosus <vespers-garages.0n@icloud.com>
2024-02-27 13:10:01 -0500

    added home comp

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,20 @@
 export { default as About } from './About/about.jsx';
 
 export { default as Intro } from './Intro/intro.jsx';
+
 export { default as Contact } from './Contact/contact.jsx';
+
 export { default as FAQ } from './FAQ/faq.jsx';
+
 export { default as NewPatient } from './NewPatient/newPatient.jsx';
+
 export { default as Services } from './Services/services.jsx';
+
 export { default as Prenatal } from './Services/Prenatal/prenatal.jsx';
+
 export { default as Pediatric } from './Services/Pediatric/pediatric.jsx';
+
 export { default as Family } from './Services/Family/family.jsx';
+
+export { default as Home } from './Home/home.jsx';
 
