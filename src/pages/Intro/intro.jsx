602b16b13f1ea3d08c7a6cbd19e78cc2338f3c05
sempercuriosus <vespers-garages.0n@icloud.com>
2024-02-28 21:34:43 -0500

    added container for the hero and a palceholder image

--- src/pages/Intro/intro.jsx
+++ src/pages/Intro/intro.jsx
@@ -1,11 +1,50 @@
+import { colors } from '../../styles/styles';
+
+const backgroundImageUrl = '/public/hero-placeholder.jpeg';
+
 function Intro() {
   return (
     <>
-      <section className='section'>
-        <h1 className='title is- has-text-centered'>Welcome! </h1>
-        <h2 className='title is-3 has-text-centered'>
-          We are Stateline Family Chiropractic
-        </h2>
+      <section className='hero section'>
+        <div
+          className='hero is-medium'
+          style={{
+            backgroundImage: `url(${backgroundImageUrl})`,
+            backgroundPosition: 'center top',
+            backgroundSize: 'cover',
+          }}>
+          <div className='hero-head'></div>
+          <div className='hero-body'>
+            <div className='container is-fullhd'>
+              <div
+                className='content'
+                style={{
+                  backgroundColor: colors.LIGHT,
+                  borderRadius: '6px',
+                  padding: '1.25em',
+                }}>
+                <h1
+                  className='title is- has-text-centered'
+                  style={{
+                    color: colors.WHITE,
+                  }}>
+                  Welcome!{' '}
+                </h1>
+                <h2
+                  className='title is-3 has-text-centered'
+                  style={{
+                    color: colors.WHITE,
+                  }}>
+                  We are Stateline Family Chiropractic
+                </h2>
+              </div>
+            </div>
+          </div>
+          <div className='hero-foot'></div>
+        </div>
+
+        <div className='block'></div>
+
         <p>
           <strong>At Stateline Family Chiropractic</strong>, we strive to
           provide excellent, affordable care to families in the Fort Mill area.
