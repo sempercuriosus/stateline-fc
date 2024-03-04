a1a6597b9bc914e40bca461abe4041a5b0f55a60
Eric <vespers-garages.0n@icloud.com>
2024-03-04 12:33:46 -0500

    added new pic, updated header, split hero out

--- src/pages/Intro/intro.jsx
+++ src/pages/Intro/intro.jsx
@@ -1,49 +1,8 @@
-import { colors } from '../../styles/styles';
-
-const backgroundImageUrl = '/images/hero-placeholder.jpeg';
-
 function Intro() {
   return (
     <>
-      <section className='hero section'>
-        <div
-          className='hero is-medium'
-          style={{
-            backgroundImage: `url(${backgroundImageUrl})`,
-            backgroundPosition: 'center top',
-            backgroundSize: 'cover',
-          }}>
-          <div className='hero-head'></div>
-          <div className='hero-body'>
-            <div className='container is-fullhd'>
-              <div
-                className='content'
-                style={{
-                  backgroundColor: colors.LIGHT,
-                  borderRadius: '6px',
-                  padding: '1.25em',
-                }}>
-                <h1
-                  className='title is- has-text-centered'
-                  style={{
-                    color: colors.WHITE,
-                  }}>
-                  Welcome!{' '}
-                </h1>
-                <h2
-                  className='title is-3 has-text-centered'
-                  style={{
-                    color: colors.WHITE,
-                  }}>
-                  We are Stateline Family Chiropractic
-                </h2>
-              </div>
-            </div>
-          </div>
-          <div className='hero-foot'></div>
-        </div>
-
-        <div className='block'></div>
+      <section>
+        <h1 className='title'>Welcome!</h1>
 
         <p>
           <strong>At Stateline Family Chiropractic</strong>, we strive to
